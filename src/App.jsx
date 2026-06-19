c02dae87a8ee3716a763624c6de24fceb06a73c0
Keerthana7654 <keerthanar8532@gmail.com>
2026-06-19 09:19:56 +0530

    Added Backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import MainLayout from "./MainLayout";
-import Explore    from "./Explore";
-import AdminPage  from "./AdminPage";
+import Explore from "./Explore";
+import AdminPage from "./AdminPage";
 
 function AppRoutes() {
   const location = useLocation();
+
   return (
     <Routes>
-      <Route path="/"        element={<MainLayout key={location.key} />} />
+      <Route path="/" element={<MainLayout key={location.key} />} />
       <Route path="/explore" element={<Explore />} />
-      <Route path="/admin"   element={<AdminPage />} />
+      <Route path="/admin" element={<AdminPage />} />
     </Routes>
   );
 }
 
 export default function App() {
-  return (
-      <AppRoutes />
-  );
+  return <AppRoutes />;
 }
