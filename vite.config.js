a44f1a9909657a1862ae6632e8d77fa5a6b7a627
Keerthana7654 <keerthanar8532@gmail.com>
2026-06-19 08:23:11 +0530

    dded backend

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/emotion-adaptive-ui/', // ← Change to '/' if using custom domain or Vercel
+  base: '/emotion-adaptive-ui/', 
 })
