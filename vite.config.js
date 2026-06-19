74cd5d6d09ebed07bc96c580004b4ba1971f3ee2
Keerthana7654 <keerthanar8532@gmail.com>
2026-06-19 08:46:09 +0530

    Added backend

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/emotion-adaptive-ui/', 
+  base: '/AI-Emotion-Adaptive-UI-Dynamic-Frontend-that-changes-based-on-user-mood-/'
 })
